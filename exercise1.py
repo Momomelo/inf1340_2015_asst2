3043b34118adaa9df7b0a45e7e3552092c23548a
Momomelo <y.kei1ro@gmail.com>
2015-11-05 22:22:52 -0500

    Exercise 1 abides by all PEP-8 regulations

--- exercise1.py
+++ exercise1.py
@@ -13,25 +13,29 @@ __license__ = "MIT License"
 
 
 def pig_latinify(word):
-	""" This function will make the inputted argument into piglatin.
+    """ This function will make the inputted argument into piglatin.
 
-	    :param word: a string or an int
-		:return: a new string
-		:raise: TypeError:
-			When more than one variabe is inputted.
-	"""
+        :param word: a string or an int
+        :return: a new string
+        :raise: TypeError:
+            When more than one variabe is inputted.
+    """
 
-	vowel = ["a","e","i","o","u"]
-	consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
-	word = str(word)
-	word = word.lower()
+    vowel = ["a", "e", "i", "o", "u"]
+    consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+                 "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
-	if word[0] in vowel:
-		return word + "yay"
-	elif word[0] in consonant:
-		while word[0] in consonant:
-			word = word + word[0]
-			word = word[1:]
-		return word + "ay"
-	else:
-		return ("Error. Does not start with vowel or consonant.")
+    # Converting word to a string so integers are accepted.
+    word = str(word)
+    # Convert word to lowercase so capitalization is irrelevant.
+    word = word.lower()
+
+    if word[0] in vowel:
+        return word + "yay"
+    elif word[0] in consonant:
+        while word[0] in consonant:
+            word = word + word[0]
+            word = word[1:]
+        return word + "ay"
+    else:
+        return "Error. Does not start with vowel or consonant."
